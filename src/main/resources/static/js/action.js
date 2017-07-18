f392db90334e8c85ecb8cbc3cb916883b21ec4e7
K-K <1354333986@qq.com>
2017-07-18 14:06:50 +0800

    界面更新 (#1)
    result界面处理/css 、js独立

---
+++ src/main/resources/static/js/action.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2017/7/18.
+ */
+$("#welcom").click(function(){
+    var welcom=$("#welcom");
+    var h=$("#h");
+    var hh=$("#hh");
+    welcom.animate({top:'350px'},1000);
+    h.animate({fontSize:'1.5em',opacity:'0'},500);
+    h.animate({fontSize:'1em',opacity:'0'},500);
+    h.animate({opacity:'1'},700);
+    hh.animate({fontSize:'0.5em',opacity:'0'},500);
+    hh.animate({fontSize:'0.3em',opacity:'0'},100);
+    hh.animate({opacity:'1'},700);
+
+});
+$("#welcom").click(function(){
+    var shade=$("#shade");
+    shade.animate({height:'768px',opacity:'0.5'},1300);
+
+});
+$("#welcom").click(function(){
+    var region=$("#region");
+    region.animate({top:'500',opacity:'1'},1500);
+
+});
