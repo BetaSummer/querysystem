590a94a1c1e5607dc10105a14b5d5d8ee0d744e4
KKHdu <1354333986@qq.com>
2017-07-20 13:33:15 +0800

    界面改进

--- src/main/resources/static/js/action.js
+++ src/main/resources/static/js/action.js
@@ -6,13 +6,13 @@ window.onload=function(){
     var welcom=$("#welcom");
     var shade=$("#shade");
     welcom.animate({top:'350px',opacity:'0',width:'-=15%',marginLeft:'+=7.5%'},0);
-    welcom.animate({top:'350px',opacity:'1'},500);
-    shade.animate({opacity:'0.3'},500);
-    region.animate({top:'500',opacity:'1'},500);
+    welcom.animate({top:'350px',opacity:'1'},1000);
+    shade.animate({opacity:'0.3'},1000);
+    region.animate({top:'500',opacity:'1'},800);
     var user=$("#user");
     var shadee=$("#shadee");
     user.animate({top:'100px',opacity:'0.3'},0);
     user.animate({top:'0px',opacity:'1'},800);
-    shadee.animate({opacity:'0.3'},500);
+    shadee.animate({opacity:'0.3'},800);
 };
 
